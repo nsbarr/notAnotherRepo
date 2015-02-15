f29b8e0ee380b0164b74304cfc75188840c8deb7
nsbarr <nsbarr@gmail.com>
2015-02-15 16:12:49 -0500

    swinging my fiery sword

---
+++ CameraTutorial/PageItemController.swift
@@ -0,0 +1,36 @@
+//
+//  PageItemController.swift
+//  Paging_Swift
+//
+//  Created by Olga Dalton on 26/10/14.
+//  Copyright (c) 2014 swiftiostutorials.com. All rights reserved.
+//
+
+import UIKit
+
+class PageItemController: UIViewController {
+    
+    // MARK: - Variables
+    var itemIndex: Int = 1 {
+        didSet {
+            
+        }
+    }
+//    var imageName: String = "" {
+        
+//        didSet {
+//            
+//            if let imageView = contentImageView {
+//                imageView.image = UIImage(named: imageName)
+//            }
+//            
+//        }
+//    }
+    
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       // contentImageView!.image = UIImage(named: imageName)
+    }
+}
--- CameraTutorial/ViewController.swift
+++ CameraTutorial/ViewController.swift
@@ -7,101 +7,41 @@
 //
 
 import UIKit
-import AVFoundation
 import CoreData
 
-class ViewController: UIViewController, UIGestureRecognizerDelegate {
-
-    let session = AVCaptureSession()
-    var previewLayer : AVCaptureVideoPreviewLayer?
+class ViewController: UIViewController, UIPageViewControllerDataSource {
     
-    var backCameraDevice:AVCaptureDevice?
-    var frontCameraDevice:AVCaptureDevice?
-    var stillCameraOutput:AVCaptureStillImageOutput!
-    
-    var sessionQueue = dispatch_queue_create("com.example.camera.capture_session", DISPATCH_QUEUE_SERIAL)
-    
+    var pageViewController: UIPageViewController?
     
     var l8rs = [NSManagedObject]()
-    var l8rCount = Int()
-    
-    var snapButton: UIButton!
     var inboxButton: UIButton!
     var inboxNumber: UILabel!
-    var dateButton: UIButton!
-    var scheduleButton: UIButton!
-    var deleteButton: UIButton!
     
+    //MARK: - Lifecycle
     
-    // If we find a device we'll store it here for later use
-    var captureDevice : AVCaptureDevice?
-    
-    
-    
-    
+    //1 fetch l8rs from core data
+    //2 create pageController
+    //3 set up first itemController, which is the Camera
+    //misc. generate itemControllers on the fly for l8rs in inbox
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.fetchL8rs()
+        self.createPageViewController()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let availableCameraDevices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
-        for device in availableCameraDevices as [AVCaptureDevice] {
-            if device.position == .Back {
-                backCameraDevice = device
-            }
-            else if device.position == .Front {
-                frontCameraDevice = device
-            }
-        }
         
-        var error:NSError?
-        let possibleCameraInput: AnyObject? = AVCaptureDeviceInput.deviceInputWithDevice(backCameraDevice, error: &error)
-        if let backCameraInput = possibleCameraInput as? AVCaptureDeviceInput {
-            if self.session.canAddInput(backCameraInput) {
-                self.session.addInput(backCameraInput)
-            }
-        }
-        
-        stillCameraOutput = AVCaptureStillImageOutput()
-        
-        if self.session.canAddOutput(self.stillCameraOutput) {
-            self.session.addOutput(self.stillCameraOutput)
-        }
-        
-        //this auto-handles focus, WB, exposure, etc.
-        session.sessionPreset = AVCaptureSessionPresetPhoto
-
-        
-        previewLayer = AVCaptureVideoPreviewLayer(session: session)
-        self.view.layer.addSublayer(previewLayer)
-        previewLayer?.frame = view.bounds
-        previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
-        
-        dispatch_async(sessionQueue) { () -> Void in
-            self.session.startRunning()
-        }
-        
-        self.addGestureRecognizer()
-        self.addSnapButton()
     }
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        
+    func fetchL8rs(){
         let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
-        
         let managedContext = appDelegate.managedObjectContext!
-        
         let fetchRequest = NSFetchRequest(entityName: "L8R")
-        
         var error: NSError?
-        
         let fetchedResults = managedContext.executeFetchRequest(fetchRequest, error: &error) as [NSManagedObject]?
         
         if let results = fetchedResults {
             l8rs = results
-            l8rCount = l8rs.count
             println("Number of l8rs:\(l8rs.count)")
             
         }
@@ -110,20 +50,31 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         }
     }
     
-    
-    func addSnapButton(){
-        snapButton = UIButton(frame: CGRect(x: 0, y: view.frame.height-100, width: 80, height: 80))
-        snapButton.center.x = view.center.x
-        let buttonImage = UIImage(named: "snapButton")
-        snapButton.setImage(buttonImage, forState: .Normal)
-        snapButton.addTarget(self, action: Selector("snapButtonPressed:"), forControlEvents: .TouchUpInside)
-        view.addSubview(snapButton)
+    func createInboxBadge(){
+
+
+    }
+
+    func createPageViewController() {
         
-        inboxButton = UIButton(frame: CGRectMake(20,self.view.frame.height-60, 40, 40))
+        //create PageViewController
+        let pageController = self.storyboard!.instantiateViewControllerWithIdentifier("PageController") as UIPageViewController
+        pageController.dataSource = self
+        
+        //create cameraController and set as first page
+        let cameraController = self.storyboard!.instantiateViewControllerWithIdentifier("CameraController") as CameraController
+        let startingViewControllers: NSArray = [cameraController]
+        pageController.setViewControllers(startingViewControllers, direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+        
+
+        pageViewController = pageController
+        addChildViewController(pageViewController!)
+        
+        inboxButton = UIButton(frame: CGRectMake(self.view.frame.width - 60, 60, 40, 40))
         inboxButton.addTarget(self, action: Selector("openInbox:"), forControlEvents:UIControlEvents.TouchUpInside)
         let inboxButtonImage = UIImage(named: "inboxButton")
         inboxButton.setImage(inboxButtonImage, forState: .Normal)
-        view.addSubview(inboxButton)
+        pageViewController!.view.addSubview(inboxButton)
         
         
         //stupid. don't initialize in header, and shows 0 because this happens before viewwillappear
@@ -131,158 +82,77 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         inboxNumber.center.x = inboxButton.center.x + 15
         inboxNumber.font = UIFont(name: "Arial", size: 18)
         inboxNumber.textColor = UIColor.whiteColor()
-        inboxNumber.text = String(l8rCount)
-        view.addSubview(inboxNumber)
+        inboxNumber.text = String(l8rs.count)
+        pageViewController!.view.addSubview(inboxNumber)
         
-        dateButton = UIButton(frame: CGRectMake(20, self.view.frame.height-60, 116, 42))
-        dateButton.addTarget(self, action: Selector("openDateMenu:"), forControlEvents: UIControlEvents.TouchUpInside)
-        dateButton.center.x = self.view.center.x
-        let dateButtonImage = UIImage(named: "tomorrowButton")
-        dateButton.setImage(dateButtonImage, forState: .Normal)
-        dateButton.hidden = true
-        view.addSubview(dateButton)
-        
-        scheduleButton = UIButton(frame: CGRectMake(self.view.frame.width-78, self.view.frame.height-60, 58, 42))
-        scheduleButton.addTarget(self, action: Selector("scheduleL8r:"), forControlEvents: UIControlEvents.TouchUpInside)
-        let scheduleButtonImage = UIImage(named: "scheduleButton")
-        scheduleButton.setImage(scheduleButtonImage, forState: .Normal)
-        scheduleButton.hidden = true
-        view.addSubview(scheduleButton)
-        
-        deleteButton = UIButton(frame: CGRectMake(20, self.view.frame.height-60, 42, 42))
-        deleteButton.addTarget(self, action: Selector("deleteL8r:"), forControlEvents: UIControlEvents.TouchUpInside)
-        let deleteButtonImage = UIImage(named: "deleteButton")
-        deleteButton.setImage(deleteButtonImage, forState: .Normal)
-        deleteButton.hidden = true
-        view.addSubview(deleteButton)
+        self.view.addSubview(pageViewController!.view)
+        pageViewController!.didMoveToParentViewController(self)
     }
     
-    func openInbox(sender: UIButton){
-        
-    }
+    // MARK: - UIPageViewControllerDataSource
     
-    func openDateMenu(sender: UIButton){
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
         
-    }
-    
-    func deleteL8r(sender: UIButton){
         
-        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
-        let managedContext = appDelegate.managedObjectContext!
-        let fetchRequest = NSFetchRequest(entityName: "L8R")
-        var error: NSError?
-        let fetchedResults = managedContext.executeFetchRequest(fetchRequest, error: &error) as [NSManagedObject]?
-        if let results = fetchedResults {
-            l8rs = results
+        
+        if viewController.restorationIdentifier == "CameraController" {
+            println("nothing before camera")
+            return nil // Camera is always first
         }
-        else {
-            println("Could not fetch \(error), \(error!.userInfo)")
-        }
-        managedContext.deleteObject(l8rs[l8rs.count-1])
-        println("just deleted a l8r")
         
-        previewLayer?.connection.enabled = true
-        self.swapInNewButtons()
-
-    }
-    
-    
-    func scheduleL8r(sender: UIButton){
-        self.swapInNewButtons()
-        self.previewLayer?.connection.enabled = true
-    }
-    
-    func addGestureRecognizer(){
-        let dragUp = UIPanGestureRecognizer(target: self, action: Selector("draggedUp:"))
-        self.view.addGestureRecognizer(dragUp)
-    }
-    
-    func draggedUp(sender: UIPanGestureRecognizer){
-        sender.delegate = self
-        println("dragged")
-    }
-    
-    func snapButtonPressed(sender: UIButton){
-        println("snapped")
-        
-        dispatch_async(sessionQueue) { () -> Void in
-            
-            let connection = self.stillCameraOutput.connectionWithMediaType(AVMediaTypeVideo)
-            
-            // update the video orientation to the device one
-            connection.videoOrientation = AVCaptureVideoOrientation(rawValue: UIDevice.currentDevice().orientation.rawValue)!
-            
-            self.stillCameraOutput.captureStillImageAsynchronouslyFromConnection(connection) {
-                (imageDataSampleBuffer, error) -> Void in
-                
-                if error == nil {
-                    
-                    // if the session preset .Photo is used, or if explicitly set in the device's outputSettings we get the data already compressed as JPEG
-                    
-                    // freeze display
-                    self.previewLayer?.connection.enabled = false
-                    
-                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
-                    
-                    // the sample buffer also contains the metadata, in case we want to modify it
-                    let metadata:NSDictionary = CMCopyDictionaryOfAttachments(nil, imageDataSampleBuffer, CMAttachmentMode(kCMAttachmentMode_ShouldPropagate)).takeUnretainedValue()
-                    
-                    let currentDate = NSDate()
-                    
-                    
-                    if let image = UIImage(data: imageData) {
-                        // save the image or do something interesting with it
-                        
-                        self.saveImageWithData(imageData, fireDate:currentDate)
-                        self.swapInNewButtons()
-                        //  do this if you want to save the Image
-                      //  UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-                        
-                        //problems with this: 1) delay, 2) image is not saved at view bounds
-//                        let imageView = UIImageView(frame:self.view.frame)
-//                        imageView.image = image
-//                        self.view.addSubview(imageView)
-                        
-                    }
-                }
-                else {
-                    NSLog("error while capturing still image: \(error)")
-                }
+        else { // we got a PageItemController
+            var itemController = viewController as PageItemController
+            println(itemController.itemIndex)
+            if itemController.itemIndex == 1 { //If we got the first ItemController, then go back to Camera
+                println("this is the top of the item stack, going back to cam")
+                let cameraController = self.storyboard!.instantiateViewControllerWithIdentifier("CameraController") as CameraController
+                return cameraController
             }
+            else {
+                println("showing previous item")
+                return getItemController(itemController.itemIndex-1) //Otherwise go back to previous ItemController
+            }
+        
+            
         }
     }
     
-    func saveImageWithData(data:NSData, fireDate:NSDate){
-     //   println("l8r data:\(data)")
-     //   println("l8r fireData:\(fireDate)")
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
         
-        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        println("VC is of Type:\(viewController.restorationIdentifier)")
         
-        let managedContext = appDelegate.managedObjectContext!
         
-        let entity = NSEntityDescription.entityForName("L8R", inManagedObjectContext: managedContext)
-        
-        let l8r = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
-        
-        l8r.setValue(data, forKey: "imageData")
-        l8r.setValue(fireDate, forKey: "fireDate")
-        
-        var error: NSError?
-        if !managedContext.save(&error) {
-            println("Coulnd't save \(error), \(error?.userInfo)")
+        if viewController.restorationIdentifier == "CameraController" {
+            println("going from cam to top of item stack")
+            return getItemController(1) // top of Inbox
         }
         
-        l8rs.append(l8r)
+        else {
+            var itemController = viewController as PageItemController
+            println("trying to leave item controller with index\(itemController.itemIndex)")
+
+            if itemController.itemIndex+1 < l8rs.count {
+                return getItemController(itemController.itemIndex+1)
+            }
+            println("bottom of the stack")
+            return nil
+        }
+        
     }
     
-    func swapInNewButtons(){
+    private func getItemController(itemIndex: Int) -> PageItemController? {
         
-        for button in [snapButton, inboxButton, inboxNumber, scheduleButton, dateButton, deleteButton] {
-            button.hidden = !button.hidden
+        if itemIndex < l8rs.count {
+            let pageItemController = self.storyboard!.instantiateViewControllerWithIdentifier("ItemController") as PageItemController
+            pageItemController.itemIndex = itemIndex
+       //     pageItemController.imageName = contentImages[itemIndex]
+            return pageItemController
         }
         
-        
+        return nil
     }
+    
+
 
 }
 
